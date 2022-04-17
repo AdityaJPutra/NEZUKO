f5b1e9760a878497d0bc6fda9be84623928d8b97
AdityaJPutra <96516589+AdityaJPutra@users.noreply.github.com>
2022-04-17 19:42:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
-global.packname = 'Bot Whatsapp'
-global.author = 'Nezuko'
+global.owner = ['62881023471561','0881023471561']
+global.packname = 'MillBot'
+global.author = 'AdityaJPutra'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'AdityaJPutra'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
